4bfa9088e5879b81e20a7275a3d3ecd42a674986
filipesjesus <47498794+filipesjesus@users.noreply.github.com>
2019-06-07 08:50:12 -0300

    Update init.sh

--- 3.3/init.sh
+++ 3.3/init.sh
@@ -14,7 +14,7 @@ sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-d
 sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim" && \
 useradd -m -p password1234 nominatim && \
 chown -R nominatim:nominatim ./src && \
-echo "Diretorio osm pbf: $OSMFILE" \
-sudo -u nominatim ./src/build/utils/setup.php --osm-file /home/e224215/nominatimdata/brazil-latest.osm.pbf --all --threads $THREADS && \
+echo "Parametros: [$OSMFILE] [$PGDIR] [$THREADS]" \
+sudo -u nominatim ./src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS && \
 sudo -u postgres /usr/lib/postgresql/11/bin/pg_ctl -D /data/$PGDIR stop && \
 sudo chown -R postgres:postgres /data/$PGDIR
